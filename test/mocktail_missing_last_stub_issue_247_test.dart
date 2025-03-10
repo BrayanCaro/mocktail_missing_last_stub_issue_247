f0495301381dece9e3fff75682ad1a4911f01fe0
Brayan Martínez <bmartinez@solopago.com>
2025-03-10 17:02:44 -0600

    Add tests for Home and SleepyHome lifecycle behaviors

--- test/mocktail_missing_last_stub_issue_247_test.dart
+++ test/mocktail_missing_last_stub_issue_247_test.dart
@@ -1,8 +1,61 @@
-import 'package:mocktail_missing_last_stub_issue_247/mocktail_missing_last_stub_issue_247.dart';
 import 'package:test/test.dart';
+import 'package:mocktail/mocktail.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test("Home should invoke sleep lifecycle once", () {
+    final animal = Animal();
+    final home = Home(animal);
+
+    when(() => animal.sleep()).thenAnswer((_) async => 'zzz');
+    when(() => animal.wakeUp()).thenThrow(Exception());
+
+    expect(() async => await home.awake(), throwsA(isA<Exception>()));
+
+    logInvocations([animal]);
+
+    verify(() => animal.sleep()).called(1);
+    verify(() => animal.wakeUp()).called(1);
+
+    verifyNoMoreInteractions(animal);
   });
+
+  test("SleepyHome should throw when animal cannot wake up", () {
+    final animal = Animal();
+    final home = SleepyHome(animal);
+
+    when(() => animal.wakeUp()).thenThrow(Exception('bad'));
+
+    expect(() async => await home.wakeUp(), throwsA(isA<Exception>()));
+
+    logInvocations([animal]);
+
+    verify(() => animal.wakeUp()).called(1);
+
+    verifyNoMoreInteractions(animal);
+  });
+}
+
+class Home {
+  final Animal animal;
+
+  Home(this.animal);
+
+  Future<void> awake() async {
+    await animal.sleep();
+    await animal.wakeUp();
+  }
+}
+
+class SleepyHome {
+  final Animal animal;
+
+  SleepyHome(this.animal);
+
+  Future<void> wakeUp() => animal.wakeUp();
+}
+
+class Animal extends Mock {
+  Future<String> sleep();
+
+  Future<String> wakeUp();
 }
