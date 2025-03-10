39917dd6994c766a6178ae05bb5a823943976a6b
Brayan Martínez <bmartinez@solopago.com>
2025-03-10 16:34:25 -0600

    initial commit

---
+++ bin/mocktail_missing_last_stub_issue_247.dart
@@ -0,0 +1,5 @@
+import 'package:mocktail_missing_last_stub_issue_247/mocktail_missing_last_stub_issue_247.dart' as mocktail_missing_last_stub_issue_247;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mocktail_missing_last_stub_issue_247.calculate()}!');
+}
---
+++ test/mocktail_missing_last_stub_issue_247_test.dart
@@ -0,0 +1,8 @@
+import 'package:mocktail_missing_last_stub_issue_247/mocktail_missing_last_stub_issue_247.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
